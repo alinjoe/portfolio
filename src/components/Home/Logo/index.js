dd824736bb85070c4851d7d26de1cb71c1cdb666
AlinJoe <alinjoe98@gmail.com>
2023-07-17 09:55:04 +0530

    chanes

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -4,6 +4,9 @@ import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
+
+
+
 const Logo = () => {
   const bgRef = useRef()
   const outlineLogoRef = useRef()
